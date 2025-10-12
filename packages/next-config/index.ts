02042a702f87cab4a1ff926a9397dd237dc1aa65
AlphaLawless <alpha.vylly12@gmail.com>
2025-10-11 23:51:27 -0300

    chore: update packages

--- packages/next-config/index.ts
+++ packages/next-config/index.ts
@@ -1,10 +1,6 @@
 import withBundleAnalyzer from '@next/bundle-analyzer';
-
-// @ts-expect-error No declaration file
-import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
 import type { NextConfig } from 'next';
 
-const otelRegex = /@opentelemetry\/instrumentation/;
 
 export const config: NextConfig = {
   images: {
@@ -33,17 +29,6 @@ export const config: NextConfig = {
         destination: 'https://us.i.posthog.com/decide',
       },
     ];
-  },
-
-  webpack(config, { isServer }) {
-    if (isServer) {
-      config.plugins = config.plugins || [];
-      config.plugins.push(new PrismaPlugin());
-    }
-
-    config.ignoreWarnings = [{ module: otelRegex }];
-
-    return config;
   },
 
   // This is required to support PostHog trailing slash API requests
